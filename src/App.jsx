cef1e6823cc531b324ce9341a384e92ab090b46f
ilham <ilham2004t@gmail.com>
2023-03-23 12:23:50 +0600

    headerear

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ import Nav from "./components/Nav/Nav";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Slider } from "./components/Slider/Slider";
+import imghed1 from "./assets/headerse.png";
+import imghed2 from "./assets/headerse1.png";
+import imghed3 from "./assets/headerse2.png";
+import imghed4 from "./assets/headerse3.png";
+
 import { Container } from "./shared/Container/index";
+import { HeaderCard } from "./components/HeaderCard/HeaderCard";
 
 function App() {
   return (
@@ -18,6 +24,7 @@ function App() {
         <NavContact />
       </div>
       <Slider />
+      <HeaderCard />
     </div>
   );
 }
