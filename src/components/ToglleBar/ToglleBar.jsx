6fb0ec472b8f470fa95fd8f4ea294f065f2fd5e0
ilham <ilham2004t@gmail.com>
2023-03-29 11:25:18 +0600

    kjh

--- src/components/ToglleBar/ToglleBar.jsx
+++ src/components/ToglleBar/ToglleBar.jsx
@@ -8,10 +8,26 @@ export const ToglleBar = () => {
       <div>
         <h1 className={styles.title}>ЧАСТО ЗАДОВАЕМЫЕ ВОПРОСЫ </h1>
         <div className={styles.item}>
+          <input className={styles.inp} type="checkbox" name="inp" id="inp" />
           <label for="inp" className={styles.title1}>
             Могу ли я купить сертификат в подарорк?
           </label>
-          <input className={styles.inp} type="radio" name="inp" id="inp" />
+
+          <div className={styles.text}>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam fuga
+              vitae eaque veritatis, totam qui quisquam sapiente eligendi
+              consequuntur repellat minus sed dolore, saepe ex similique
+              dolores, necessitatibus deleniti ut?
+            </p>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <input className={styles.inp} type="checkbox" name="inp" id="inp2" />
+          <label for="inp2" className={styles.title1}>
+            Могу ли я купить сертификат в подарорк?
+          </label>
+
           <div className={styles.text}>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam fuga
